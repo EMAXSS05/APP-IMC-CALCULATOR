027ccf2a2ad09e0231f7fa4cba3a0950f8718969
emanuel <a24emanuelrb@iessanclemente.net>
2025-11-19 09:49:18 +0100

    cambios guardados

--- app/src/main/java/com/example/appimccalculator/ImcCalculatorFragment.kt
+++ app/src/main/java/com/example/appimccalculator/ImcCalculatorFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import androidx.compose.ui.semantics.text
+
 
 class ImcCalculatorFragment : Fragment() {
 
